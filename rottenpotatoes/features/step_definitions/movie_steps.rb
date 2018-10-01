efdfe3847888cc0cafa0b8593028618efb57058d
Jonathan Grimes <nsa@tamu.edu>
2018-10-01 04:27:11 +0000

    Part 1 maybe completed?

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,7 @@
+Given(/^the following movies exist:$/) do |table|
+  movies_table.hashes.each do |movie|; Movie.create!(movie) end
+end
+
+Then(/^the director of "([^"]*)" should be "([^"]*)"$/) do |arg1, arg2|
+  assert page.body =~ /#{arg1}.+Director.+#{arg2}/m
+end
